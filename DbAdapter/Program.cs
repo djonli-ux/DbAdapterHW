5ed4f88fb0030f285c1a41c282f216ee47183524
Jonatan Lifshits <djonli2003@gmail.com>
2023-07-22 01:53:34 +0300

    add: app.config + mod: connection program.cs

--- DbAdapter/Program.cs
+++ DbAdapter/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+string connectionString = "Server=DESKTOP-TO8EGSF;Database=P01_dump;Trusted_Connection=True;Encrypt=False;"; 
+DataSet ds = new DataSet();
+
+
+using SqlConnection conn = new SqlConnection(connectionString);
+try
+{
+	conn.Open();
+    Console.WriteLine("Connection ok...");
+
+}
+catch (Exception)
+{
+
+	throw;
+}
+
